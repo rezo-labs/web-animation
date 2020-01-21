09e1fb2fcc03379cf9a89e11455c8e2cdbf5c414
Lộc Trần <mrloctran19@gmail.com>
2020-01-21 18:23:34 +0700

    add firework, add countdown

--- src/happy-new-year-2020/js/index.js
+++ src/happy-new-year-2020/js/index.js
@@ -1,2 +1,34 @@
 console.log('hello world');
 console.log('test browser sync');
+// const anime = require('animejs');
+const box = anime({
+    targets: '.box',
+    translateX: 300,
+    backgroundColor: '#FFF',
+    duration: 1000,
+    loop: true,
+    direction: 'alternate',
+    borderRadius: ['0%', '50%'],
+    easing: 'easeInOutQuad',
+    autoplay: false,
+})
+
+const drawMouse = anime({
+    targets: '.mouse path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    delay: function(el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true,
+    autoplay: false
+  });
+
+document.querySelector('.play').onclick = box.play;
+document.querySelector('.pause').onclick = box.pause;
+
+document.querySelector('.draw').onclick = drawMouse.play;
+document.querySelector('.draw-pause').onclick = drawMouse.pause;
+
+
+
