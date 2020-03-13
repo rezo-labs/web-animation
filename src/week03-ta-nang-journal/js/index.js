d9fa29b549da59a5ef28376b175c4594599ef8a0
duy <duy>
2020-03-13 19:33:09 +0700

    add animation: paragraph, images reveal

--- src/week03-ta-nang-journal/js/index.js
+++ src/week03-ta-nang-journal/js/index.js
@@ -4,6 +4,19 @@ var INCREMENT = 100 / img.length;
 const PAGE_REVEAL_TIME = 1000;
 const DELAY_BEFORE_PAGE_REVEAL = 1000;
 const GAP = 300;
+const TIME_TO_SHOW_IMAGE = 1500;
+
+const ARRAY_POST = [
+    {
+        title: "Đi lạc lần thứ 1",
+        description:
+            "Vừa mới đặt chân tới Tà Năng cả nhóm đã bị lạc. Nguyên nhân bị lạc là vì libero ngu. Sau một hồi lạc trong rừng 30 phút thì cũng tìm được cách đi ra. không hiểu sao lại có một thằng ngu như vậy trong đoàn..."
+    },
+    {
+        title: "Hết đi lạc rồi",
+        description: "Tìm được đường về rồi vui qúa ba má ơi."
+    }
+];
 
 // @read https://stackoverflow.com/questions/3877027/jquery-callback-on-image-load-even-when-the-image-is-cached
 // select het tat ca hinh
@@ -15,10 +28,14 @@ function handleLoad() {
             // neu load xong thi cap nhat tien do
             updateProgress();
             console.log(this);
+
             // cap nhat animation
             if (PROGRESS > 99) {
                 console.log("load xong");
-                setTimeout(() => startReveal(),DELAY_BEFORE_PAGE_REVEAL);
+                setTimeout(() => startReveal(), DELAY_BEFORE_PAGE_REVEAL);
+                //
+                setTimeout(() => showContent(), 2000);
+                setTimeout(() => showParaghaph(), 3000);
             }
         })
         .each(function() {
@@ -33,7 +50,8 @@ function handleLoad() {
 
 function updateProgress() {
     PROGRESS = PROGRESS + INCREMENT;
-    $("#colored-logo").css("clip-path", `inset(0 ${100 - PROGRESS}% 0 0 )`);
+
+    $("#colored-logo").css("clip-path", `inset(0 ${100 - PROGRESS}% 0 0)`);
     console.log("progress", PROGRESS);
 }
 
@@ -42,7 +60,10 @@ function setCenterBlock() {
     var X = window.innerWidth / 2 - 175;
     var Y = window.innerHeight / 2 - 140;
 
-    console.log("X", { X: X, Y: Y });
+    console.log("X", {
+        X: X,
+        Y: Y
+    });
 
     // select reveal block = grey-logo + colored logo
     var REVEAL_BLOCK = $("#revealBlock").get("0");
@@ -67,6 +88,7 @@ function updateProgressImage() {
 
 function startReveal() {
     console.log("START REVEAL");
+
     //move to top
     anime({
         targets: "#revealBlock",
@@ -82,10 +104,10 @@ function startReveal() {
     // change background to white
     anime({
         targets: "body",
-        backgroundColor: "#FFF",
+        backgroundColor: "#EAEBEA",
         easing: "easeInOutSine",
-        duration: 500,
-        delay: PAGE_REVEAL_TIME+GAP
+        duration: 1000,
+        delay: PAGE_REVEAL_TIME + 2 * GAP
     });
 
     // Reveal black logo
@@ -94,7 +116,7 @@ function startReveal() {
         opacity: 1,
         easing: "easeInOutSine",
         duration: 500,
-        delay: PAGE_REVEAL_TIME,
+        delay: PAGE_REVEAL_TIME
     });
 
     // hide loading
@@ -103,7 +125,64 @@ function startReveal() {
         opacity: 0,
         easing: "easeInOutSine",
         duration: 500,
-        delay: PAGE_REVEAL_TIME,
+        delay: PAGE_REVEAL_TIME
+    });
+}
+
+/**FLOW SHOW MAIN */
+/**Show Image Collection */
+
+// @read https://tobiasahlin.com/moving-letters/
+/**Show Content */
+function showContent() {
+    console.log("START SHOW CONTENT");
+
+    anime
+        .timeline({
+            targets: ".wrapImages",
+            // translateX: ["0", 100], // from 100 to 250
+
+            delay: 0,
+            // easing: "cubicBezier(.5, .05, .1, 1)",
+            opacity: 0,
+
+            // rotate: 360,
+            direction: "reverse",
+            easing: "linear",
+            duration: 400
+        })
+        .add({
+            targets: ".wrapImages",
+            translateY: ["0", 20]
+        })
+        .add({
+            targets: ".wrapImages",
+            // easing: "easeOutQuad",
+            scale: 2
+        });
+
+    anime({
+        targets: "#img-2",
+        easing: "cubicBezier(.5, .05, .1, 1)",
+        rotate: 15,
+        translateX: ["0", 50],
+        duration: TIME_TO_SHOW_IMAGE
+    });
+
+    anime({
+        targets: "#img-3",
+        easing: "cubicBezier(.5, .05, .1, 1)",
+        rotate: -15,
+        translateX: ["0", -30],
+        duration: TIME_TO_SHOW_IMAGE + 1000
+    });
+
+    anime({
+        targets: "#img-4",
+        easing: "cubicBezier(.5, .05, .1, 1)",
+        rotate: -20,
+        translateY: ["0", -20],
+        duration: TIME_TO_SHOW_IMAGE + 2000
     });
 }
 
@@ -118,3 +197,115 @@ handleLoad();
 
 // Ham StartReveal se cho handleLoad()
 // load xong thi goi StartReveal()
+
+// smooth scrolling:
+// initialize
+function init() {
+    new SmoothScroll(document.body, 110, 10);
+}
+
+// handle smooth scroll event:
+function SmoothScroll(target, speed, smooth) {
+    if (target === document)
+        target =
+            document.scrollingElement ||
+            document.documentElement ||
+            document.body.parentNode ||
+            document.body; // cross browser support for document scrolling
+
+    var moving = false;
+    var pos = target.scrollTop;
+    var frame =
+        target === document.body && document.documentElement
+            ? document.documentElement
+            : target; // safari is the new IE
+
+    target.addEventListener("mousewheel", scrolled, {
+        passive: false
+    });
+
+    target.addEventListener("DOMMouseScroll", scrolled, {
+        passive: false
+    });
+
+    function scrolled(e) {
+        e.preventDefault(); // disable default scrolling
+
+        var delta = normalizeWheelDelta(e);
+
+        pos += -delta * speed;
+        pos = Math.max(
+            0,
+            Math.min(pos, target.scrollHeight - frame.clientHeight)
+        ); // limit scrolling
+
+        if (!moving) update();
+    }
+
+    function normalizeWheelDelta(e) {
+        if (e.detail) {
+            if (e.wheelDelta)
+                return (e.wheelDelta / e.detail / 40) * (e.detail > 0 ? 1 : -1);
+            // Opera
+            else return -e.detail / 3; // Firefox
+        } else return e.wheelDelta / 120; // IE,Safari,Chrome
+    }
+
+    function update() {
+        moving = true;
+
+        var delta = (pos - target.scrollTop) / smooth;
+
+        target.scrollTop += delta;
+
+        if (Math.abs(delta) > 0.5) requestFrame(update);
+        else moving = false;
+    }
+
+    var requestFrame = (function() {
+        // requestAnimationFrame cross browser
+        return (
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function(func) {
+                window.setTimeout(func, 1000 / 50);
+            }
+        );
+    })();
+}
+
+//
+// call smooth scrolling:
+// $(document).ready(init());
+
+var textWrapper = document.querySelector(".description");
+var textWrapper1 = document.querySelector(".description-1");
+textWrapper.innerHTML = ARRAY_POST[0].description.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+);
+
+function showParaghaph() {
+    anime
+        .timeline({
+            loop: false
+        })
+        .add({
+            targets: ".description .letter",
+            scale: [4, 1],
+            opacity: 1,
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 950,
+            delay: (el, i) => 50 * i
+        });
+}
+
+function showTitle() {
+    anime({
+        targets
+    });
+}
