8ec5f350a973cdf3817bcafe83b0dba868e28853
duy <duy>
2020-03-15 23:31:43 +0700

    add event change title/description for btn next/prev

--- src/week03-ta-nang-journal/js/event.js
+++ src/week03-ta-nang-journal/js/event.js
@@ -1,33 +1,39 @@
-console.log('EVENT')
+console.log("EVENT");
 const DEBOUNCE_TIME = 1000;
 const THROTTLE_TIME = 1000;
 
 // PREV TRACKPOINT BUTTON
-$("#prev--btn").click(throttle(function() {
-    let currentItem = sessionStorage.getItem('currentItem');
-    console.log(currentItem);
-    if (currentItem == 0) return 0;
-    // decrease 1 value
-    currentItem = parseInt(currentItem) - 1
-    sessionStorage.setItem('currentItem', currentItem);
-    console.log(ARRAY_POST[currentItem]);
-}, THROTTLE_TIME));
+$("#prev--btn").click(
+    throttle(function() {
+        let currentItem = sessionStorage.getItem("currentItem");
+        console.log(currentItem);
+        if (currentItem == 0) return 0;
+        // decrease 1 value
+        currentItem = parseInt(currentItem) - 1;
+        sessionStorage.setItem("currentItem", currentItem);
+        console.log(ARRAY_POST[currentItem]);
+        $(".title").text(`${ARRAY_POST[currentItem].title}`);
+        $(".description").text(`${ARRAY_POST[currentItem].description}`);
+    }, THROTTLE_TIME)
+);
 
 // NEXT TRACKPOINT BUTTON
-$("#next--btn").click(throttle(function() {
-    let currentItem = sessionStorage.getItem('currentItem');
-    console.log(currentItem);
-    if (currentItem >= 10) return 0;
-    // decrease 1 value
-    currentItem = parseInt(currentItem) + 1
-    sessionStorage.setItem('currentItem', currentItem);
-    console.log(ARRAY_POST[currentItem]);
-}, THROTTLE_TIME));
-
+$("#next--btn").click(
+    throttle(function() {
+        let currentItem = sessionStorage.getItem("currentItem");
+        console.log(currentItem);
+        if (currentItem >= 10) return 0;
+        // decrease 1 value
+        currentItem = parseInt(currentItem) + 1;
+        sessionStorage.setItem("currentItem", currentItem);
+        console.log(ARRAY_POST[currentItem]);
+        $(".title").text(`${ARRAY_POST[currentItem].title}`);
+        $(".description").text(`${ARRAY_POST[currentItem].description}`);
+    }, THROTTLE_TIME)
+);
 
 // Chuyen qua moc ke tiep
 function trackPointTransition() {
     // Animation when CURRENT item move out
-
     // Animation when NEW item move in
 }
