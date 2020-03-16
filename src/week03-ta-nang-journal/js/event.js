db6314e31acfd0ebc55f9d4129eb57d634a5dd7d
duy <duy>
2020-03-16 17:41:31 +0700

    add image animation in prev/next screen

--- src/week03-ta-nang-journal/js/event.js
+++ src/week03-ta-nang-journal/js/event.js
@@ -1,6 +1,7 @@
 console.log("EVENT");
 const DEBOUNCE_TIME = 1000;
 const THROTTLE_TIME = 1000;
+// const TIME_TO_SHOW_IMAGE = 1500;
 
 // PREV TRACKPOINT BUTTON
 $("#prev--btn").click(
@@ -15,6 +16,7 @@ $("#prev--btn").click(
         // add new class move-out
         $(".title").addClass("move-out");
         $(".description").addClass("move-out");
+        $(".wrapImages").empty();
         moveOut();
         // append new node
         $(".block-title").append(
@@ -23,6 +25,17 @@ $("#prev--btn").click(
         $(".block-description").append(
             `<p class="description">${TRACKLOG[currentItem].description}</p>`
         );
+        $(`.wrapImages`).append(() => {
+            let imgArray = "";
+            for (let i = 1; i <= 4; i++) {
+                imgArray += `
+                    <img src="${
+                        TRACKLOG[currentItem].image_collections[i - 1]
+                    }" alt="" id="img-${i}">
+                `;
+            }
+            return imgArray;
+        });
     }, THROTTLE_TIME)
 );
 
@@ -39,6 +52,8 @@ $("#next--btn").click(
         // add new class move-out
         $(".title").addClass("move-out");
         $(".description").addClass("move-out");
+        $(".wrapImages").empty();
+        console.log($(".wrapImages img"));
         moveOut();
         // append new node
         $(".block-title").append(
@@ -47,6 +62,17 @@ $("#next--btn").click(
         $(".block-description").append(
             `<p class="description">${TRACKLOG[currentItem].description}</p>`
         );
+        $(`.wrapImages`).append(() => {
+            let imgArray = "";
+            for (let i = 1; i <= 4; i++) {
+                imgArray += `
+                    <img src="${
+                        TRACKLOG[currentItem].image_collections[i - 1]
+                    }" alt="" id="img-${i}">
+                `;
+            }
+            return imgArray;
+        });
     }, THROTTLE_TIME)
 );
 
@@ -74,6 +100,62 @@ function moveOut() {
             $(".move-out").remove();
         }
     });
+    setTimeout(() => {
+        showContent();
+    }, 1);
+    showContent();
+}
+
+function showContent() {
+    console.log("START SHOW CONTENT");
+
+    anime
+        .timeline({
+            targets: ".wrapImages",
+            // translateX: ["0", 100], // from 100 to 250
+
+            delay: 0,
+            // easing: "cubicBezier(.5, .05, .1, 1)",
+            opacity: [1, 0],
+
+            // rotate: 360,
+            direction: "reverse",
+            easing: "linear",
+            duration: 500
+        })
+        .add({
+            targets: ".wrapImages",
+            translateY: ["0", 20]
+        })
+        .add({
+            targets: ".wrapImages",
+            // easing: "easeOutQuad",
+            scale: [2, 1]
+        });
+
+    anime({
+        targets: "#img-2",
+        easing: "cubicBezier(.5, .05, .1, 1)",
+        rotate: 15,
+        translateX: ["0", 50],
+        duration: TIME_TO_SHOW_IMAGE
+    });
+
+    anime({
+        targets: "#img-3",
+        easing: "cubicBezier(.5, .05, .1, 1)",
+        rotate: -15,
+        translateX: ["0", -30],
+        duration: TIME_TO_SHOW_IMAGE + 1000
+    });
+
+    anime({
+        targets: "#img-4",
+        easing: "cubicBezier(.5, .05, .1, 1)",
+        rotate: -20,
+        translateY: ["0", -20],
+        duration: TIME_TO_SHOW_IMAGE + 2000
+    });
 }
 
 // var moveTitleOut = anime({
@@ -84,13 +166,24 @@ function moveOut() {
 // Chuyen qua moc ke tiep
 let childIndex = "";
 console.log("progressbar: ", $(".progressbar li .dotStage"));
-const dotStageArray = $(".progressbar").on("click", "li", () => {
-    // console.log($(this).index()){
-    // const parent = $(dotStageArray)
-    //     .parent()
-    //     .parent();
-    // childIndex = $(parent).index();
-    console.log($(dotStageArray));
+// const dotStageArray = $(".progressbar").on("click", "li", () => {
+// console.log($(this).index()){
+// const parent = $(dotStageArray)
+//     .parent()
+//     .parent();
+// childIndex = $(parent).index();
+// console.log($(dotStageArray));
+// });
+$(document).ready(() => {
+    $(".progressbar li .dotStage").click(event => {
+        console.log(
+            "index: ",
+            // $(".progressbar")
+            //     .children()
+            //     .index()
+            this.event
+        );
+    });
 });
 // Chuyen qua moc k)e tiep
 function trackPointTransition() {
